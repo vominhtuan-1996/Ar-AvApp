17d9a783274e24c688e006cad8393a5099fd964c
TuanIOS_SU12 <TuanVM37@fpt.com.vn>
2022-12-01 11:14:31 +0700

    #base Structure

---
+++ MageApp/MageApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MageApp
+//
+//  Created by TuanIOS_SU12 on 01/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
